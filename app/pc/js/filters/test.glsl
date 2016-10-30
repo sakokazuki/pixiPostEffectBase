ffb14f9f88d2897a381464579d0e438042eec556
sako <sako@aid-dcc.com>
2016-10-30 14:47:46 +0900

    thremo

--- app/pc/js/filters/test.glsl
+++ app/pc/js/filters/test.glsl
@@ -10,11 +10,23 @@ uniform vec2 resolution;
 uniform float col;
 uniform vec2 mouse;
 
+vec3 hsv2rgb(vec3 c){
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 void main() {
-  vec2 coord = vTextureCoord;
+  float aspect = resolution.y / resolution.x;
+  vec2 uv = vTextureCoord;
+  vec4 col = texture2D(uSampler, uv);
+  float gray = (col.r + col.g + col.b) / 3.0;
 
-  vec4 color = texture2D(uSampler, coord);
-  color.x += .4;
-  gl_FragColor = vec4(color);
+  vec3 thremo;
+  thremo = hsv2rgb(vec3(gray, 1., 1.));
+
+  gl_FragColor = vec4(thremo, 1.);
 }
+
+
+
